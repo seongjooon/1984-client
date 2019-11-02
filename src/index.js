3ff43278c2a390f66cd6f8d54e30548a78e71288
MrKamake <seongjooon@gmail.com>
2019-11-02 15:37:48 +0900

    Set up a redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
+import App from './container/App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const rootEL = document.getElementById('root');
+
+render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  rootEL
+);
+
 serviceWorker.unregister();
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+export const initState = {};
+
+export default combineReducers({});
