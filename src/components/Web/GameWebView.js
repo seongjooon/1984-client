41278067926e7c2f0b3a22bd669e213a8cb366e5
MrKamake <seongjooon@gmail.com>
2019-11-13 17:51:10 +0900

    create crash feature from obstacle

--- src/components/Web/GameWebView.js
+++ src/components/Web/GameWebView.js
@@ -61,7 +61,11 @@ class GameWebView extends Component {
   };
 
   _updateGame = unitList => {
-  
+    for (let i = 1; i < unitList.length; i++) {
+      if (this._crashWith([unitList[0], unitList[i]])) {
+        this._stopGame();
+      }
+    }
     this._clearCanvas();
 
     for (let i = 0; i < unitList.length; i++) {
