010561e5c0b357aeed4b969a1d4ad53451586b7a
MrKamake <seongjooon@gmail.com>
2019-11-13 20:45:01 +0900

    move initial obstacles to constants file

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,3 +1,5 @@
+import { getRandomNumber } from '../utils';
+
 const isMobileDvice = window.innerHeight < 460 || window.innerHeight < 600;
 
 export const SERVER_API = isMobileDvice
@@ -9,3 +11,20 @@ export const SERVER_URL = isMobileDvice
   : 'http://localhost:5000';
 
 export const FALLING_OBSTACLE = 3;
+
+export const OBSTACLE_LIST = [];
+
+for (let i = 0; i < 10; i++) {
+  const initialObstacleSize = getRandomNumber(30);
+  const sampleObstacle = {
+    color: 'green',
+    x: 1,
+    y: -1,
+    width: initialObstacleSize / 2,
+    height: initialObstacleSize / 2
+  };
+  sampleObstacle.x *= getRandomNumber(82);
+  sampleObstacle.y *= getRandomNumber(82);
+
+  OBSTACLE_LIST.push(sampleObstacle);
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,7 @@ import { connectDeviceReducer } from './connectDeviceReducer';
 import { gameStartReducer } from './gameStartReducer';
 import { moveAirplaneReducer } from './moveAirplaneReducer';
 import { moveObstacleReducer } from './moveObstacleReducer';
-import { getRandomNumber } from '../utils';
+import { OBSTACLE_LIST } from '../constants/constant';
 
 export const initState = {
   isMobileDevice: false,
@@ -17,24 +17,8 @@ export const initState = {
     width: 8,
     height: 7
   },
-  obstacles: []
+  obstacles: OBSTACLE_LIST
 };
-
-const initialObstacleSize = getRandomNumber(30);
-
-for (let i = 0; i < 8; i++) {
-  const sampleObstacle = {
-    color: 'green',
-    x: 1,
-    y: -1,
-    width: initialObstacleSize / 2,
-    height: initialObstacleSize / 2
-  };
-  sampleObstacle.x *= getRandomNumber(82);
-  sampleObstacle.y *= getRandomNumber(82);
-
-  initState.obstacles.push(sampleObstacle);
-}
 
 export default combineReducers({
   isMobileDevice: checkIsMobileDeviceReducer,
